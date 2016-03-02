cf7a34ab3248c81d421e2354572b6993f49f60a5
Adrian Sieber <mail@adriansieber.com>
2016-03-02 11:45:03 +0100

    Pass-through config options to lowdb

--- index.js
+++ index.js
@@ -3,20 +3,32 @@
 const lowdb = require('lowdb')
 const yaml = require('js-yaml')
 
-const storage = require('lowdb/file-async')
+const asyncFileStorage = require('lowdb/file-async')
 
 const yamlFormat = {
 	serialize: yaml.safeDump,
-	deserialize: yaml.safeLoad
+	deserialize: yaml.safeLoad,
+}
+
+const defaultConfig = {
+	storage: asyncFileStorage,
+	format: yamlFormat,
+	writeOnChange: true,
+	databaseName: 'ybdb',
 }
 
 module.exports = class Ybdb {
 	constructor (configObject) {
-		console.assert(configObject.storageFile)
+		configObject = Object.assign(
+			{},
+			defaultConfig,
+			configObject
+		)
 
-		return lowdb(configObject.storageFile, {
-			storage,
-			format: yamlFormat
-		})
+		return lowdb(
+			configObject.storageFile || configObject.databaseName,
+			configObject,
+			configObject.writeOnChange
+		)
 	}
 }
