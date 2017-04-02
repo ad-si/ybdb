c85152ea55d7f1541ce754b698ebf75df7d6f067
Adrian Sieber <mail@adriansieber.com>
2017-04-02 21:02:14 +0000

    Upgrade to lowdb 0.16, use yarn for package management

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-'use strict'
-
 const lowdb = require('lowdb')
 const yaml = require('js-yaml')
 
-
 const yamlFormat = {
-	serialize: yaml.safeDump,
-	deserialize: yaml.safeLoad,
+  serialize: yaml.safeDump,
+  deserialize: yaml.safeLoad,
 }
 
 const defaultConfig = {
-	format: yamlFormat,
-	writeOnChange: true,
-	databaseName: 'ybdb',
+  format: yamlFormat,
+  writeOnChange: true,
+  databaseName: 'ybdb',
 }
 
 module.exports = class Ybdb {
-	constructor (configObject) {
+  constructor (configObject) {
+    if (!configObject) {
+      return lowdb()
+    }
 
-		if (!configObject) {
-			return lowdb()
-		}
+    configObject = Object.assign(
+      {},
+      defaultConfig,
+      configObject
+    )
 
-		configObject = Object.assign(
-			{},
-			defaultConfig,
-			configObject
-		)
-
-		return lowdb(
-			configObject.storageFile || configObject.databaseName,
-			configObject,
-			configObject.writeOnChange
-		)
-	}
+    return lowdb(
+      configObject.storageFile || configObject.databaseName,
+      configObject
+    )
+  }
 }
--- tests/main.js
+++ tests/main.js
@@ -1,60 +1,78 @@
-'use strict'
-
 const fs = require('fs')
 const path = require('path')
+const assert = require('assert')
 
 const Ybdb = require('../index.js')
 
 {
-	const db = new Ybdb()
+  const db = new Ybdb()
 
-	db.object.contacts = [
-		{name: 'John', age: 45},
-		{name: 'Anna', age: 34}
-	]
+  db
+    .defaults({
+      contacts: [
+        {name: 'John', age: 45},
+        {name: 'Anna', age: 34},
+      ],
+    })
+    .write()
 
-	console.assert(
-		db('contacts').find({name: 'Anna'}).age === 34,
-		db('contacts').find({name: 'Anna'}).age + ' instead of 34'
-	)
+  const retrievedAge = db
+    .get('contacts')
+    .find({name: 'Anna'})
+    .value()
+    .age
+
+  assert.equal(
+    retrievedAge,
+    34,
+    `${retrievedAge} instead of 34`
+  )
 }
 
 {
-	const tempFile = path.join(__dirname, 'temp.yaml')
-	const referenceFile = path.join(__dirname, 'reference.yaml')
-	const asyncFileStorage = require('lowdb/file-async')
+  const tempFile = path.join(__dirname, 'temp.yaml')
+  const referenceFile = path.join(__dirname, 'reference.yaml')
+  const asyncFileStorage = require('lowdb/lib/storages/file-async')
 
-	function deleteTestFile () {
-		try {
-			fs.unlinkSync(tempFile)
-		}
-		catch (error) {}
-	}
+  function deleteTestFile () {
+    try {
+      fs.unlinkSync(tempFile)
+    }
+    catch (error) {
+      if (!error.message.includes('no such file')) console.error(error)
+    }
+  }
 
-	function readFile (filePath) {
-		return fs.readFileSync(filePath, 'utf-8')
-	}
+  function readFile (filePath) {
+    return fs.readFileSync(filePath, 'utf-8')
+  }
 
+  deleteTestFile()
 
-	deleteTestFile()
+  const db = new Ybdb({
+    storage: asyncFileStorage,
+    storageFile: tempFile,
+  })
 
-	const db = new Ybdb({
-		storage: asyncFileStorage,
-		storageFile: tempFile,
-	})
+  db
+    .defaults({
+      songs: [],
+    })
+    .write()
 
-	db('songs')
-		.chain()
-		.push({title: 'Song One'})
-		.push({title: 'Another Song'})
-		.push({title: 'The Song'})
-		.value()
-		.then(songs => {
-			console.assert(
-				readFile(tempFile) === readFile(referenceFile),
-				readFile(tempFile) + ' should equal ' + readFile(referenceFile)
-			)
-			deleteTestFile()
-		})
-		.catch(console.error)
+  db
+    .get('songs')
+    .push({title: 'Song One'})
+    .push({title: 'Another Song'})
+    .push({title: 'The Song'})
+    .write()
+    .then(songs => {
+      assert(songs)
+      assert(
+        readFile(tempFile) === readFile(referenceFile),
+        readFile(tempFile) + ' should equal ' + readFile(referenceFile)
+      )
+      deleteTestFile()
+    })
+    .catch(console.error)
 }
