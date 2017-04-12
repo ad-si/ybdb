0b74f7672edcac4d297b4dd7897a506a49ecc091
Adrian Sieber <mail@adriansieber.com>
2017-04-12 14:51:52 +0000

    Test using several directories as storage paths

---
+++ tests/multi-dirs.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const assert = require('assert')
+const Ybdb = require('../index.js')
+const expectedData = {
+  'contact-dirs': [
+    {
+      name: 'Anna Smith',
+      birthday: new Date('1978-08-22'),
+      company: 'Evil Corp',
+      localId: 'anna',
+    },
+    {
+      name: 'John Doe',
+      birthday: new Date('1962-02-15'),
+      company: 'Good Corp',
+      localId: 'john',
+    },
+  ],
+  'contact-files': [
+    {
+      name: 'Anna Smith',
+      birthday: new Date('1978-08-22'),
+      company: 'Evil Corp',
+      localId: 'anna',
+    },
+    {
+      name: 'John Doe',
+      birthday: new Date('1962-02-15'),
+      company: 'Good Corp',
+      localId: 'john',
+    },
+  ],
+}
+
+async function runTest () {
+  process.stdout.write('Test multiple YAML directories')
+
+  const database = new Ybdb({
+    storagePaths: [
+      path.join(__dirname, 'fixtures/contact-dirs'),
+      path.join(__dirname, 'fixtures/contact-files'),
+    ],
+  })
+  const initializedDb = await database.init()
+
+  assert.deepEqual(initializedDb.getState(), expectedData)
+  console.info(' ✔︎')
+}
+
+runTest()
