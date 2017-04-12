43e2d45d2fd11ee9db52a1a1c58284ffc150ffb2
Adrian Sieber <mail@adriansieber.com>
2017-04-12 14:56:10 +0000

    Test joining keys in data object generated from multiple file trees

---
+++ tests/multi-dirs-join.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const assert = require('assert')
+const Ybdb = require('../index.js')
+const expectedData = [
+  {
+    name: 'Anna Smith',
+    birthday: new Date('1978-08-22'),
+    company: 'Evil Corp',
+    localId: 'anna',
+  },
+  {
+    name: 'John Doe',
+    birthday: new Date('1962-02-15'),
+    company: 'Good Corp',
+    localId: 'john',
+  },
+  {
+    name: 'Anna Smith',
+    birthday: new Date('1978-08-22'),
+    company: 'Evil Corp',
+    localId: 'anna',
+  },
+  {
+    name: 'John Doe',
+    birthday: new Date('1962-02-15'),
+    company: 'Good Corp',
+    localId: 'john',
+  },
+]
+
+async function runTest () {
+  process.stdout.write('Join multiple YAML file trees')
+
+  const database = new Ybdb({
+    storagePaths: [
+      path.join(__dirname, 'fixtures/contact-dirs'),
+      path.join(__dirname, 'fixtures/contact-files'),
+    ],
+    joined: true,
+  })
+  const initializedDb = await database.init()
+
+  assert.deepEqual(initializedDb.getState(), expectedData)
+  console.info(' ✔︎')
+}
+
+runTest()
